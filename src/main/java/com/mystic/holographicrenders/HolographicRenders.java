b795ac9e6bc5c97c7fe6f3b4361e9bc7c304ca23
Mysticpasta1 <ethan.cantrell@asub.edu>
2021-05-27 15:22:33 -0500

    Start of a new project

---
+++ src/main/java/com/mystic/holographicrenders/HolographicRenders.java
@@ -0,0 +1,10 @@
+package com.mystic.holographicrenders;
+
+import net.fabricmc.api.ModInitializer;
+
+public class HolographicRenders implements ModInitializer {
+	@Override
+	public void onInitialize() {
+
+	}
+}
