2799c2aac8894ad4f30c8e8c23296872084f1239
glisco03 <glisco03@web.de>
2021-06-01 17:41:45 +0200

    Area scanner outline

--- src/main/java/com/mystic/holographicrenders/mixin/WorldRendererMixin.java
+++ src/main/java/com/mystic/holographicrenders/mixin/WorldRendererMixin.java
@@ -6,6 +6,8 @@ import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.render.*;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Matrix4f;
@@ -24,21 +26,24 @@ public class WorldRendererMixin {
         final ClientPlayerEntity player = client.player;
         if (player.getMainHandStack().getItem() != HolographicRenders.AREA_SCANNER) return;
 
-
         CompoundTag tag = player.getMainHandStack().getOrCreateTag();
 
         if (!tag.contains("Pos1")) return;
 
         BlockPos origin = BlockPos.fromLong(tag.getLong("Pos1"));
-        BlockPos target = tag.contains("Pos2") ? BlockPos.fromLong(tag.getLong("Pos2")) : new BlockPos(player.raycast(player.abilities.creativeMode ? 5.0F : 4.5F, 0, false).getPos());
+
+        HitResult result = player.raycast(player.abilities.creativeMode ? 5.0F : 4.5F, 0, false);
+        BlockPos size = tag.contains("Pos2") ? BlockPos.fromLong(tag.getLong("Pos2")) : (result instanceof BlockHitResult ? ((BlockHitResult) result).getBlockPos() : new BlockPos(result.getPos()));
+        size = size.subtract(origin);
+
+        origin = origin.add(size.getX() < 0 ? 1 : 0, size.getY() < 0 ? 1 : 0, size.getZ() < 0 ? 1 : 0);
+        size = size.add(size.getX() >= 0 ? 1 : -1, size.getY() >= 0 ? 1 : -1, size.getZ() >= 0 ? 1 : -1);
 
         matrices.push();
 
         VertexConsumer consumer = client.getBufferBuilders().getEntityVertexConsumers().getBuffer(RenderLayer.getLines());
-
-        matrices.translate(-origin.getX() - camera.getPos().x, -origin.getY() - camera.getPos().y, -origin.getZ() - camera.getPos().z);
-        //System.out.println("draw");
-        WorldRenderer.drawBox(matrices, consumer, new Box(0, 0, 0, 5, 5, 5), 1, 1, 1, 1);
+        matrices.translate(origin.getX() - camera.getPos().x, origin.getY() - camera.getPos().y, origin.getZ() - camera.getPos().z);
+        WorldRenderer.drawBox(matrices, consumer, new Box(BlockPos.ORIGIN, size), 0.25f, 0.25f, 1, 1);
 
         matrices.pop();
     }
